ac1e1c3f0c6046d27cbe7e1985b1b9182744af68
Bejong Yang <bejongyang@gmail.com>
2019-11-24 01:31:35 -0600

    Init commit. Blank Base Project.

---
+++ src/components/Landing.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Landing extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
